7c9aec53ffcea83ed380b3995174dbfa12504662
DESKTOP-0T8J1S3\Martijn <mjsboom@gmail.com>
2019-01-21 15:15:34 +0100

    Started work on prover. Added "Kids love super mario" example.

--- Prover.py
+++ Prover.py
@@ -1,20 +1,72 @@
 from LoLaLinkNode import *
+from Graph import *
+import itertools
 
 class Prover:
     def __init__(self):
-        print("gegroet!")
+        print("init prover")
 
-    def prove(self, sequent, targetType):
-        # derivations = []
-        # build the proof nets
-        # connect the trees
-        # for each connection:
-        #   # keep applying contractions and structural rules until stuck
-        #   # if it is a tensor tree, add to derivations
-        #   # else go to next connection
-        # for each derivation:
-        #   # return the proof net and the proof term
-        return True
+    def prove(self, sentence, lexicon, targetType):
+        words = sentence.lower().split()
+        forms = map(lambda x: lexicon[x], words)
+        #unfoldedGraphs = list(map(lambda x: stanfunctie[x], forms))
+
+        g1 = LoLaGraph()
+        g2 = LoLaGraph()
+        g3 = LoLaGraph()
+
+        g1.addNode(NODE_FACTORY.createVertex(g1, "np"))
+        l1 = g2.addNode(NODE_FACTORY.createLinkNode(g2))
+        l2 = g2.addNode(NODE_FACTORY.createLinkNode(g2))
+        n1 = g2.addNode(NODE_FACTORY.createVertex(g2, "(np\s)/np"))
+        n2 = g2.addNode(NODE_FACTORY.createVertex(g2, "np"))
+        n3 = g2.addNode(NODE_FACTORY.createVertex(g2, "np\s"))
+        n4 = g2.addNode(NODE_FACTORY.createVertex(g2, "np"))
+        n5 = g2.addNode(NODE_FACTORY.createVertex(g2, "s"))
+        g2.addEdge(l1, n1)
+        g2.addEdge(l1, n2)
+        g2.addEdge(n3, l1)
+        g2.addEdge(l2, n3)
+        g2.addEdge(l2, n4)
+        g2.addEdge(n5, l2)
+
+        g3.addNode(NODE_FACTORY.createVertex(g3, "s"))
+
+        unfoldedGraphs = [g1,g2,g3]
+
+        dingen = g1.getPossibleConnections(g2)
+
+        for ding in dingen:
+            print("hoi")
+
+        # unfolded = wordh.makevertexunfold
+        # Find all possible graphs obtained from connecting
+
+        # unfoldedGraphs = [1,2,3]
+        # perms = list(itertools.permutations(unfoldedGraphs))
+        #
+        # for perm in perms:
+        #     accumulatedGraphs = [perm[0]]
+        #     for i in range(1, len(perm)):
+        #         for aGraph in accumulatedGraphs:
+        #             otherGraph = perm[i]
+        #             aGraph.getPossibleConnections(otherGraph)
+
+
+
+        derivations = []
+        while graphs:
+            graph = graphs.pop()
+            if graph.isTensorTree():
+                derivations.append(graph)
+                continue
+
+            graphs = graphs + graph.getPossibleContractions()
+            graphs = graphs + graph.getPossibleRewritings()
+
+        for derivation in derivations:
+            # return the proof term
+            print("ik ben een derivation")
 
     def buildGraph(self):
         return True
